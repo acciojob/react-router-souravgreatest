b953d2b18943ad4ba3336b7f0645022290f8b153
souravgreatest <jainsourav220@gmail.com>
2025-04-29 20:27:38 +0530

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
 import React from "react";
 import './../styles/App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Navigation from './Navigation';
 
-const App = () => {
+function App() {
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <BrowserRouter>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
+
